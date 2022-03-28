c9d1ab0f0e85cdb48862abaff91b0d5f33458034
dangdinhphuong1 <2051063780@e.tlu.edu.com>
2022-03-28 10:18:46 +0700

    Khoi tao phan lam viec cua Dinh Phuong

--- JS/main.js
+++ JS/main.js
@@ -23,7 +23,8 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("sectionid").style.padding = "30px 10px 30px 10px";
+    document.getElementById("sectionid").style.maxWidth = "100%";
+    document.getElementById("sectionid").style.padding = "30px 10px";
     document.getElementById("sectionid").style.background = "#00283f";
   } else {
     document.getElementById("sectionid").style.padding = "60px 40px 60px 40px";
