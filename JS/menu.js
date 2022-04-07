73b283fd83bf08fc036d275aafee817020eba833
NguyenCongPhong81 <2051063551@e.tlu.edu.vn>
2022-04-07 23:08:39 +0700

    Update reponsive index

--- JS/menu.js
+++ JS/menu.js
@@ -1,18 +1,36 @@
-
 window.onscroll = function () {
-    scrollFunction();
-  };
+  scrollFunction();
+};
 function scrollFunction() {
-    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-      document.getElementById("header-nav").style.marginTop = "-180px";
-      document.getElementById("header-nav").style.marginLeft = "-18px";
-      document.getElementById("header-nav").style.paddingTop = "70px";
-      document.getElementById("header-nav").style.background = "#00283f";
-      document.getElementById("header-nav").style.position = "fixed";
-      document.getElementById("header-nav").style.width = "100vw";
-    } else {
-        document.getElementById("header-nav").style.marginTop = "0px";
-      document.getElementById("header-nav").style.padding = "0px 40px 0px 40px";
-      document.getElementById("header-nav").style.background = "none";
-    }
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    document.getElementById("header-nav").style.marginTop = "-180px";
+    document.getElementById("header-nav").style.marginLeft = "-18px";
+    document.getElementById("header-nav").style.paddingTop = "70px";
+    document.getElementById("header-nav").style.background = "#00283f";
+    document.getElementById("header-nav").style.position = "fixed";
+    document.getElementById("header-nav").style.width = "100vw";
+  } else {
+    document.getElementById("header-nav").style.marginTop = "0px";
+    document.getElementById("header-nav").style.padding = "0px 40px 0px 40px";
+    document.getElementById("header-nav").style.background = "none";
   }
+}
+document.getElementById("btn-open").onclick= function(){
+  document.getElementById("header-nav").style.display="block";
+  document.getElementById("header-nav").style.background = "#00283f";
+}
+
+
+
+// function myFunction() {
+//   document.getElementById("header-nav").classList.toggle("show");
+// }
+
+// window.onclick = function(e) {
+//   if (!e.target.matches('#btn-open')) {
+//   var myDropdown = document.getElementById("header-nav");
+//     if (myDropdown.classList.contains('show')) {
+//       myDropdown.classList.remove('show');
+//     }
+//   }
+// }
