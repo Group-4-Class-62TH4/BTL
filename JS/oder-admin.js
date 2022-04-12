afdec3894aca6e9b8af4876ef440e006934c33eb
dangdinhphuong1 <2051063780@e.tlu.edu.com>
2022-04-12 16:58:03 +0700

    complete order-admin

--- JS/oder-admin.js
+++ JS/oder-admin.js
@@ -1,33 +1,24 @@
-const ipName = document.getElementById("ip-name").value;
-const ipDay = document.getElementById("ip-day").value;
-const ipPeople = document.getElementById("ip-people").value;
-const ipPhoneNumber = document.getElementById("ip-phone-number").value;
-const ipHour = document.getElementById("ip-hour").value;
-// const table = document.getElementById("myTable");
-
-// const row = table.insertRow(0);
-// const cell1 = row.insertCell(0);
-// const cell2 = row.insertCell(1);
-// const cell3 = row.insertCell(2);
-// const cell4 = row.insertCell(3);
-// const cell5 = row.insertCell(4);
-// const cell6 = row.insertCell(5);
-document.getElementById('confirm').onclick = function addRow(){
-    // const listData = [];
+const dataList = [];
+function addRow(){
+    const ipName = $("#ip-name").val();
+    const ipDay = $("#ip-day").val();
+    const ipPeople = $("#ip-people").val();
+    const ipPhoneNumber = $("#ip-phone-number").val();
+    const ipHour = $("#ip-hour").val();
     const row = [];
     row.push(ipName);
     row.push(ipPhoneNumber);
     row.push(ipHour);
     row.push(ipDay);
     row.push(ipPeople);
-    // listData.push(row);
-    // console.log(listData);
-    return row;
-
+    dataList.push(row);
+    console.log(row);
+    console.log(dataList);
+    localStorage.setItem('data', dataList);
+    // JSON.stringify(dataList)
+    $("#ip-name").val('');
+    $("#ip-day").val('');
+    $("#ip-people").val('');
+    $("#ip-phone-number").val('');
+    $("#ip-hour").val('');
 }
-//     cell1.innerHTML = table.rows.length + 1;
-//     cell2.innerHTML = ipName;
-//     cell3.innerHTML = ipPhoneNumber;
-//     cell4.innerHTML = ipHour;
-//     cell5.innerHTML = ipDay;
-//     cell6.innerHTML = "Delivered";
